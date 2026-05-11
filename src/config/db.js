b1973122bd444b6c89fbc7483c4832b008278d5d
Ogunwoolu Abdulmatin <ogunwoolua@gmail.com>
2026-05-11 13:54:49 +0100

    Completed backend setup and MongoDB connection

---
+++ src/config/db.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const dns = require("dns");
+
+const connectDB = async () => {
+  try {
+    const mongoUri = process.env.MONGO_URI;
+
+    if (!mongoUri) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
+
+    if (mongoUri.startsWith("mongodb+srv://")) {
+      dns.setServers(["1.1.1.1", "8.8.8.8"]);
+      console.log("Using public DNS for SRV resolution:", dns.getServers());
+    }
+
+    console.log(mongoUri);
+
+    await mongoose.connect(mongoUri, {
+      serverSelectionTimeoutMS: 10000,
+      socketTimeoutMS: 45000,
+      connectTimeoutMS: 10000,
+      family: 4,
+    });
+
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
