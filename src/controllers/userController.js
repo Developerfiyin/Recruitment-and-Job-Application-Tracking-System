8934a3414e85bcc8318ebd81ef49032a2932ddb2
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-05-13 20:48:11 +0100

    update  user logic for getting all  user .

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,11 +39,37 @@ exports.createUser = async (req, res, next) => {
 exports.getAllUsers = async (req, res, next) => {
   try {
     const users = await userSchema.find().sort({ name: 1 });
-    res.status(200).json({message: "Users fetched successfully", users});
+    res.status(200).json({ message: "Users fetched successfully", users });
   } catch (error) {
     next(error);
     res
       .status(500)
-      .json({ message: "An error occurred while fetching all users", data: error });
+      .json({
+        message: "An error occurred while fetching all users",
+        data: error,
+      });
+      console.error("Error fetching all users:", error);
+  }
+};
+
+exports.getUserById = async (req, res, next) => {
+  const id = req.params;
+
+  try {
+    const user = await userSchema.findById(id);
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User fetched successfully", user });
+  } catch (error) {
+    next(error);
+    res
+      .status(500)
+      .json({
+        message: "An error occurred while fetching the user by ID",
+        data: error,
+      });
+      console.error("Error fetching user by ID:", error);
   }
 };
