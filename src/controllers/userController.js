4e7485a8da39f3ffb352450ba325a9aea72cf39c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-05-13 21:08:29 +0100

    create  user logic for updating  user ny id .

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -74,6 +74,33 @@ exports.getUserById = async (req, res, next) => {
   }
 };
 
+exports.updateUserById = async (req, res, next) => {
+    const id = req.params;
+    const { name, email, password, role } = req.body;
+
+    try {
+        const updatedUser = await userSchema.findByIdAndUpdate(
+            id,
+            { name, email, password, role },
+            { new: true }
+        );
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json({ message: "User updated successfully", updatedUser });
+    } catch (error) {
+        next(error);
+        res
+            .status(500)
+            .json({
+                message: "An error occurred while updating the user",
+                data: error,
+            });
+            console.error("Error updating user:", error);
+    }
+};
+
+
 exports.deleteUser = async (req, res, next) => {
     const id = req.params;
         try {
